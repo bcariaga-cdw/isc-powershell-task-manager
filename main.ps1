000e9c2cd63110937427e5b282a64beeca494c74
bcariaga-cdw <braden.cariaga@cdw.com>
2024-08-27 12:53:22 -0400

    Update Main

--- main.ps1
+++ main.ps1
@@ -1,6 +1,9 @@
-$script:SCRIPT_PATH = "C:\SailPoint\SailPoint Scripts"
+$script:SCRIPT_PATH = "C:\Users\bradcar\OneDrive - CDW\Documents\Projects\Personal\QUEUE\queue-scripts"
 
 $runtime = Measure-Command -Expression { 
+    <#######################################################################
+    ############          Import Dependencies and Setup         ############
+    #######################################################################>
     try {
         # Declare Global Script Variables
         $script:GLOBAL_CONFIG = (Get-Content -path "$($script:SCRIPT_PATH)\config.json" | ConvertFrom-Json)
@@ -28,171 +31,177 @@ $runtime = Measure-Command -Expression {
         exit
     }
 
-    try {
-        Write-Log "Starting script processing..."
+    <#######################################################################
+    ############                   Main Script                  ############
+    #######################################################################>
+    function Main () {
+        try {
+            # Initialize Stats Class
+            $scriptStats = [ScriptStats]::new()
 
-        $script:STATS = @{
-            tasks = [System.Collections.ArrayList] @()
-            actions = [System.Collections.ArrayList] @()
-        }
+            # Fetch Open Tasks
+            $allWorkItems = [System.Collections.ArrayList] @()
+            $allWorkItems += (Invoke-Paginate -Function Get-WorkItems -Increment 250 -Limit 30000 -InitialOffset 0)
+            
+            Write-Host $allWorkItems.Count
 
-        $allTasks = [System.Collections.ArrayList](@(Get-PaginatedAndCache "work-items" Get-WorkItems @{} $false))
+            # Exit Script If No Tasks
+            if ($allWorkItems.Count -le 0 -or $null -eq $allWorkItems[0]) {
+                Write-Log "No work items found."
+                return
+            }
 
-        $tasksRun = [System.Collections.ArrayList]@()
+            # Iterate Work Items
+            foreach ($workItem in $allWorkItems) {
+                Write-Log "Processing Work Item: $($workItem.id)"
 
-        if ($allTasks.Count -gt 0) {
-            foreach ($task in $allTasks.ToArray()) {
-                Write-Log "Processing task $($task.id)"
-                $myTask = @{
-                    id = $null
-                    actions = [System.Collections.ArrayList] @()
-                    statuses = [System.Collections.ArrayList] @()
-                    transactions = [System.Collections.ArrayList] @()
-                    errors = [System.Collections.ArrayList] @()
-                }
+                # Initialize New Task Entry
+                $taskEntry = [TaskEntry]::new($null, $null, $null, $null, $null)
+                # Initalize Closability (Initial value is true and will be altered if a task can be retried.)
+                $canCloseWorkItem = $true
+                $forceCloseWorkItem = $false
 
-                $operation = $null
-                $canClose = $true
-                $actionToRemove = [System.Collections.ArrayList]@()
-                foreach ($approvalItem in $task.approvalItems) {
-                    # Check if correct source
-                    if ($approvalItem.application -ne $script:GLOBAL_CONFIG.Script.applicationName + " [source]") { continue }       
-                    # ensure is pending, add/create operation, and action
-                    if ($approvalItem.name -eq "action" -and $approvalItem.operation -ne "Create" -and $approvalItem.operation -ne "Add" -and $approvalItem.state -eq "PENDING" -and $approvalItem.operation -ne "Remove") { continue }
-                    
-                    # ensure we haven't run this before (duplicate prevention)
-                    if ($tasksRun.Contains($approvalItem.value + ":" + $approvalItem.account)) { continue }
+                # Add Statistics
+                $scriptStats.AddWorkItem($workItem.id)
 
-                    # Add statistics measurements.
-                    if (-not $script:STATS.tasks.Contains($task.id)) { [void] $script:STATS.tasks.Add($task.id) }
-                    [void] $script:STATS.actions.Add("$($task.id)-$($approvalItem.value)")
-
-                    $myTask.id = $approvalItem.account
-
-                    Write-Log "$($task.description)" 3
-
+                # Iterate the Approval Items
+                foreach ($approvalItem in $workItem.approvalItems) {
+                    # Initialize Variables for Readability
+                    $action = $approvalItem.value
+                    $account = $approvalItem.account
                     $operation = $approvalItem.operation
 
-                    # Find Current Account, if exists
-                    $currentAccount = Get-Accounts -Filters "sourceId eq `"$($script:GLOBAL_CONFIG.Script.sourceId)`" and nativeIdentity eq `"$($myTask.id)`""
-
-                    $currentStatus = $null
-                    if ($null -eq $currentAccount) {
-                        Write-Log "Current account could not be found" 1
-                    } else {
-                        # Check status of value
-                        foreach ($status in $currentAccount.attributes.statuses) {
-                            $action = $status.split(":")[0]
-                            $currStatus = ($status.split(":")[1]).Replace(" ", "")
-                            if ($action -eq $approvalItem.value) {
-                                $currentStatus = $currStatus
-                                break
-                            }
-                        }
-                    }
-
-                    if ($operation -eq "Remove" -and $null -ne $currentAccount) {
-                        [void] $mytask.statuses.Add("$($approvalItem.value): RESET")
-                        [void] $myTask.transactions.Add("$($approvalItem.value) (reset): $((Get-Date).toString("MM-dd-yyyy HH:mm:ss")) - $($(Get-Date).toString("MM-dd-yyyy HH:mm:ss"))")
-                        [void] $actionToRemove.Add($approvalItem.value)
-                        Write-Log "Removing $($approvalItem.value) from the record."
+                    # Check if correct source
+                    if ($approvalItem.application -ne $script:GLOBAL_CONFIG.Script.applicationName + " [source]") { continue }       
+                    # Ensure is pending, add/create/remove operation, and action
+                    if ($operation -ne "Create" -and $operation -ne "Add" -and $operation -ne "Remove") { continue }
+                    # Ensure we haven't run this before (duplicate prevention)
+                    if ($scriptStats.HasAction($account, $action)) { continue }
+                    # Check if needs to be closed
+                    if ($approvalItem.name -ne "actions" -or $null -eq $action) {
+                        $forceCloseWorkItem = $true
                         continue
                     }
+
+                    # Add Statistics
+                    $scriptStats.AddAction($account, $action)
+
+                    Write-Log "Processing Approval Item: $action - $account"
+
+                    # Set the id of the task to the account.
+                    $taskEntry.SetId($account)
                     
-                    # Don't run if this item is already completed.
-                    if ($currentStatus -eq "NO_RETRY" -or $currentStatus -eq "COMPLETED") {
-                        continue
-                    }
+                    # Check if the current account exists.
+                    $currentAccount = Get-Accounts -Filters "sourceId eq `"$($script:GLOBAL_CONFIG.Script.sourceId)`" and nativeIdentity eq `"$account`""
+                    if ($null -ne $currentAccount) {
+                        Write-Log "Found current account: $($currentAccount.id)"
+                        $currentAccountTaskEntry = [TaskEntry]::new($currentAccount.attributes.id, $currentAccount.attributes.actions, $currentAccount.attributes.statuses, $currentAccount.attributes.transactions, $currentAccount.attributes.errors)
+                        $currentActionStatus = $currentAccountTaskEntry.GetStatus($action)
 
-                    try {
-                        $start = (Get-Date).toString("MM-dd-yyyy HH:mm:ss")
-                        [void] (. "$($SCRIPT_PATH)\scripts\$($script:GLOBAL_CONFIG.Script.actions."$($approvalItem.value)")")
-                        $run = Run-Action $myTask.id
-                        $end = (Get-Date).toString("MM-dd-yyyy HH:mm:ss")
+                        Write-Log "Current Action Status ($action): $currentActionStatus"
 
-                        Write-Log ($run | Out-String) 3
-
-                        if ($run.status -eq "NO_RETRY" -or $run.status -eq "COMPLETED") { 
-                            [void] $myTask.actions.Add($approvalItem.value)
-                        } else {
-                            $canClose = $false
+                        # If remove, run a reset action.
+                        if ($operation -eq "Remove") {
+                            if ($currentActionStatus -ne "RESET") {
+                                Write-Log "$action has operation of $operation and will be reset." 3
+                                $taskEntry.ResetAction($action)
+                            } else {
+                                Write-Log "$action has operation of $operation and is already reset." 3
+                            }
+                            continue
                         }
-                        [void] $mytask.statuses.Add("$($approvalItem.value): $($run.status)")
-                        [void] $myTask.transactions.Add("$($approvalItem.value) ($(if ($run.status -ne "ERROR" -and $run.status -ne "NO_RETRY") {"pass"} else {"fail"})): $($start) - $($end)")
-                        if ($run.error) { [void] $myTask.errors.Add("$($approvalItem.value): $($run.error)") }
+
+                        # If current status is not to be retried, skip
+                        if ($currentActionStatus -eq "NO_RETRY" -or $currentActionStatus -eq "COMPLETED") {
+                            Write-Log "$action has status of $currentActionStatus and will not be processed." 3
+
+                            # Remove statistics since we stopped processing
+                            $scriptStats.RemoveAction($account, $action)
+                            continue
+                        }
+                    }
+
+                    $start = (Get-Date).toString("MM-dd-yyyy HH:mm:ss")
+                    # Load Run-Action method from script.
+                    [void] (. "$($script:SCRIPT_PATH)\scripts\$($script:GLOBAL_CONFIG.Script.actions."$action")")
+
+                    # Run the action.
+                    try {
+                        $run = Run-Action $taskEntry.id $start
                     } catch {
-                        Write-Log "Script failed:" 1
-                        Write-Log $_ 1
-
-                        [void] $mytask.statuses.Add("$($approvalItem.value): NO_RETRY")
-                        [void] $myTask.transactions.Add("$($approvalItem.value) (fail): $($_.Exception.transaction.start) - $($_.Exception.transaction.end)")
-                        [void] $myTask.errors.Add("$($approvalItem.value): $($_.Exception.msg)")
+                        $run = [RunResponse]::new("NO_RETRY", ($_ | Out-String))
                     }
+                    $end = (Get-Date).toString("MM-dd-yyyy HH:mm:ss")
+
+                    Write-Log "$action started on $start and ended on $end with the results of $($run.status) $(if ($null -ne $run.errorMessage) {$run.errorMessage})" 
+                    
+                    # If end runs, add action to list. 
+                    if ($run.status -eq "NO_RETRY" -or $run.status -eq "COMPLETED") { 
+                        $taskEntry.AddAction($action)
+                    } else {
+                        $canCloseWorkItem = $false
+                    }
+
+                    # Update the task entry.
+                    $taskEntry.UpdateStatus($action, $run.status)
+                    $taskEntry.AddTransaction($action, $run.status, $start, $end)
+                    if ($null -ne $run.errorMessage) { $taskEntry.AddError($action, $run.errorMessage) }
                 }
-                
-                if ($myTask.id -eq $null) { continue }
 
-                $currentAccount = Get-Accounts -Filters "sourceId eq `"$($script:GLOBAL_CONFIG.Script.sourceId)`" and nativeIdentity eq `"$($myTask.id)`""
+                # If task ID is not filled, skip. 
+                if ([string]::IsNullOrEmpty($taskEntry.id)) { 
+                    if ($forceCloseWorkItem -eq $true) {
+                        $res = Close-WorkItem -Id $workItem.id 
+                        Write-Log "Work item $($workItem.id) has been closed by force."
+                    }                    
+                    continue 
+                }
 
-                if ($operation -eq "Create" -and $null -eq $currentAccount) {
-                    Write-Log "Creating IDN record for $($myTask.id): $($myTask.actions -join ",")"
-                    Write-Log ($myTask | Out-String) 3
+                # Check if current account exists
+                $currentAccount = Get-Accounts -Filters "sourceId eq `"$($script:GLOBAL_CONFIG.Script.sourceId)`" and nativeIdentity eq `"$($taskEntry.id)`""
+                if ($null -eq $currentAccount) {
+                    # Create New Account on IDN
+                    Write-Log "Creating IDN record for $($taskEntry.id): $($taskEntry.actions -join ",")"
+                    Write-Log ($taskEntry | Out-String) 3
 
-                    $myTask.sourceId = $script:GLOBAL_CONFIG.Script.sourceId
-
-                    if ($myTask.errors.Count -eq 0) {
-                        $myTask.Remove("errors")
-                    }
-
-                    $accountAttributesCreate = Initialize-AccountAttributesCreate -Attributes $myTask
+                    $accountAttributesCreate = Initialize-AccountAttributesCreate -Attributes ($taskEntry.ConvertForCreate())
                     $res = New-Account -AccountAttributesCreate $accountAttributesCreate
 
                     Write-Log "Created account with id $($res.id)"
                 } else {
-                    Write-Log "Updating IDN record for $($myTask.id): $($myTask.actions -join ",")"
+                    # Update Account on IDN
+                    Write-Log "Found current account: $($currentAccount.id)"
+                    $currentAccountTaskEntry = [TaskEntry]::new($currentAccount.attributes.id, $currentAccount.attributes.actions, $currentAccount.attributes.statuses, $currentAccount.attributes.transactions, $currentAccount.attributes.errors)
 
-                    if ($null -eq $currentAccount) {
-                        Write-Log "Current account could not be found" 1
-                        continue
-                    }
+                    $taskEntry.Merge($currentAccountTaskEntry)
 
-                    $myTask.actions = @($($myTask.actions; ($currentAccount.attributes.actions | Where-Object { $null -ne $_ -and -not $actionToRemove.Contains($_) })))
-                    $myTask.transactions = @($($myTask.transactions; ($currentAccount.attributes.transactions | Where-Object { $null -ne $_ })))
-                    $mytask.statuses = @($($mytask.statuses; ($currentAccount.attributes.statuses | Where-Object { $null -ne $_ -and -not ($mytask.statuses -join ",").Contains(($_ -split ":")[0]) })))
-                    $myTask.errors = @($($myTask.errors; ($currentAccount.attributes.errors | Where-Object { $null -ne $_ })))
+                    Write-Log ($taskEntry | Out-String) 3
 
-                    Write-Log ($myTask | Out-String) 3
-
-                    $attributes = Initialize-AccountAttributes -Attributes $myTask
+                    $attributes = Initialize-AccountAttributes -Attributes ($taskEntry.ConvertForUpdate())
                     $res = Send-Account -Id $currentAccount.id -AccountAttributes $attributes
 
                     Write-Log "Updated account id $($myTask.id)"
                 }
 
-                if ($canClose -eq $true) {
-                    $res = Close-WorkItem -Id $task.id 
-                    Write-Log "Work item $($res.id) has been closed."
+                # Close the task
+                if ($canCloseWorkItem -eq $true) {
+                    $res = Close-WorkItem -Id $workItem.id 
+                    Write-Log "Work item $($workItem.id) has been closed."
                 } else {
-                    Write-Log "One or more actions failed and will be retried. The work item $($res.id) will not be closed."
+                    Write-Log "One or more actions failed and will be retried. The work item $($workItem.id) will not be closed."
                 }
-
-                [void] $tasksRun.Add($approvalItem.value + ":" + $approvalItem.account)
             }
 
-            Write-Log "Finished processing $($script:STATS.tasks.Count) tasks with $($script:STATS.actions.Count) actions"
-            if ($script:STATS.tasks.Count -gt 0) { Write-Log "Tasks Processed:`n$($script:STATS.tasks -join ",")" 3 }
-            if ($script:STATS.actions.Count -gt 0) { Write-Log "Actions Processed:`n$($script:STATS.actions -join ",")" 3 }
+            $scriptStats.Print()
+        } catch {
+            Write-Log ($_ | Out-String) 1
         }
-        else {
-            Write-Log "No tasks found."
-        }
-
-        Write-Log "Finished script processing."
-    }
-    catch {
-        Write-Log ($_ | Out-String) 1
     }
 
+    Write-Log "Starting script processing..."
+    # Run Main Function
+    Main
+    Write-Log "Finished script processing."
 }
 
 Write-Log "The script took $($runtime.ToString("c")) to finish."
